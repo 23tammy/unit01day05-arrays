7de5bfe6b0e61a1cd7405b89964fdcd6acd4eb00
tammy vu <tammy.vsk@gmail.com>
2023-01-17 12:38:13 -0600

    started loop

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,5 +1,11 @@
 public class ThreeArrayRange {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
+        for (int i : a ){
+            if (i >= a[0] && i >= a[1] && i >= a[2]){
+                int max = i;
+            }else 
+        }
+        System.out.println();
     }
 }
