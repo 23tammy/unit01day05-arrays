151f0d7569b6d20184f683f1ef37cf5066ab9f3a
tammy vu <tammy.vsk@gmail.com>
2023-01-18 12:12:51 -0600

    stuff

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,14 @@
 public class ImmutableSort {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
+        int[] b;
+
+        for (int i : a){
+            if (i>= a[0] && i >= a[1] && i >= a[2]){
+                
+            }
+        }
+
         
     }
 }
