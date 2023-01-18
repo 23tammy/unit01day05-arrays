dafbf5661565ce8af9ebc3542cef0ef54d28fb13
tammy vu <tammy.vsk@gmail.com>
2023-01-18 11:29:40 -0600

    fixed inititialization of variables

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,10 +1,9 @@
 public class ThreeArrayRange {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
-        
+        int min = 0;
+        int max = 0;
         for (int i : a ){
-            int min;
-            int max;
             if (i >= a[0] && i >= a[1] && i >= a[2]){
                 max = i;
             }else if (i <= a[0] && i <= a[1] && i <= a[2]){
