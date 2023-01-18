7fb63f0982e5c7144d41c9f935f6903a80d02ae7
tammy vu <tammy.vsk@gmail.com>
2023-01-18 11:35:56 -0600

    fixed loop error

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,12 +1,16 @@
 public class ThreeArrayRange {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {2, 2, 2};
         int min = 0;
         int max = 0;
         for (int i : a ){
             if (i >= a[0] && i >= a[1] && i >= a[2]){
                 max = i;
-            }else if (i <= a[0] && i <= a[1] && i <= a[2]){
+            }else{
+                continue;
+            } 
+            
+            if (i <= a[0] && i <= a[1] && i <= a[2]){
                 min = i;
             }else{
                 continue;
