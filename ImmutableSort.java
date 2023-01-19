6e93ee2e2b90b7630b9919b8694ae893ca6e674c
tammy vu <tammy.vsk@gmail.com>
2023-01-19 12:38:21 -0600

    stuff

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,4 @@
 import java.util.Arrays;
-
-
 public class ImmutableSort {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
--- MutableSort.java
+++ MutableSort.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
 public class MutableSort {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
+        for (int i : a){
+            if (i== a[0] && i == a[1] && i == a[2]){
+                a[0] = i;
+                a[1] = i;
+                a[2] = i;
+            }else if (i<= a[0] && i <= a[1] && i <= a[2]){
+                a[0] = i;
+            }else if (i>= a[0] && i <= a[2]){
+                a[1] = i;
+            }else if (i>= a[0] && i >= a[1] && i >= a[2]){
+                a[2] = i;
+            
+            
+            }
+        }
+        System.out.println(Arrays.toString(a));
     }
 }
