fa02061d70b985d59bb335a3b39925269520c6b2
tammy <tammy.vsk@gmail.com>
2023-01-19 23:16:31 -0600

    omg it works

--- MutableSort.java
+++ MutableSort.java
@@ -5,16 +5,28 @@ public class MutableSort {
         int min = 0;
         int mid = 0;
         int max = 0;
+
         for (int i : a){
             if (i== a[0] && i == a[1] && i == a[2]){
                 min = i;
                 mid = i;
                 max = i;
             }else if (i>= a[0] && i >= a[1] && i >= a[2]){
- 
+                if (max == i){
+                    mid = i;
+                }else{
+                    max = i;
+                }
             }else if (i<= a[0] && i <= a[1] && i <= a[2]){
-
+                if (min == i){
+                    mid = i;
+                }else{
+                    min = i;
+                }
+            }else{
+                mid = i;
             }
+        }
         a[0] = min;
         a[1] = mid;
         a[2] = max;
