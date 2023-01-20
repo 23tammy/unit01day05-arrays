f8f8fb7b42bd3c5ecb95da3aaf50163bdf14fdf9
tammy <tammy.vsk@gmail.com>
2023-01-19 23:05:37 -0600

    useless

--- MutableSort.java
+++ MutableSort.java
@@ -2,21 +2,22 @@ import java.util.Arrays;
 public class MutableSort {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
+        int min = 0;
+        int mid = 0;
+        int max = 0;
         for (int i : a){
             if (i== a[0] && i == a[1] && i == a[2]){
-                a[0] = i;
-                a[1] = i;
-                a[2] = i;
-            }else if (i<= a[0] && i <= a[1] && i <= a[2]){
-                a[0] = i;
-            }else if (i>= a[0] && i <= a[2]){
-                a[1] = i;
+                min = i;
+                mid = i;
+                max = i;
             }else if (i>= a[0] && i >= a[1] && i >= a[2]){
-                a[2] = i;
-            
-            
+ 
+            }else if (i<= a[0] && i <= a[1] && i <= a[2]){
+
             }
-        }
+        a[0] = min;
+        a[1] = mid;
+        a[2] = max;
         System.out.println(Arrays.toString(a));
     }
 }
