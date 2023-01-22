7174500316d0aa9c9fbb2f324bd7770f07ae0422
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-21 21:03:20 -0600

    ThreeArrayRange.java feedback

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,8 +1,10 @@
+// good
 public class ThreeArrayRange {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
         int min = 0;
         int max = 0;
+        // NOTE: 'i' is a triggering variable name in this case... 'i' almost always refers to an index (position) in an array, not an actual value from the array
         for (int i : a ){
             if (i >= a[0] && i >= a[1] && i >= a[2]){
                 max = i;
